c389ad19560989fd23f29eb0954e547bf5ba4f10
PabloHoties <pablohotiesmarques@gmail.com>
2024-07-25 12:47:59 -0300

    feat: desenvolvimento do producer da mensageria via RabbitMQ

---
+++ src/main/java/br/com/cotiinformatica/components/RabbitMQProducerComponent.java
@@ -0,0 +1,33 @@
+package br.com.cotiinformatica.components;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.cotiinformatica.dtos.MensagemUsuarioDto;
+
+@Component
+public class RabbitMQProducerComponent {
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@Autowired
+	private Queue queue;
+	
+	// Método para gravarmos um conteúdo na fila
+	public void sendMessage(MensagemUsuarioDto dto) throws Exception {
+		
+		// Serializar os dados em formato JSON
+		String json = objectMapper.writeValueAsString(dto);
+		
+		// Gravando o cnteúdo na fila
+		rabbitTemplate.convertAndSend(queue.getName(), json);
+	}
+}
--- src/main/java/br/com/cotiinformatica/services/UsuarioService.java
+++ src/main/java/br/com/cotiinformatica/services/UsuarioService.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.cotiinformatica.components.JwtTokenComponent;
+import br.com.cotiinformatica.components.RabbitMQProducerComponent;
 import br.com.cotiinformatica.components.SHA256Component;
 import br.com.cotiinformatica.dtos.AutenticarUsuarioRequestDto;
 import br.com.cotiinformatica.dtos.AutenticarUsuarioResponseDto;
 import br.com.cotiinformatica.dtos.CriarUsuarioRequestDto;
 import br.com.cotiinformatica.dtos.CriarUsuarioResponseDto;
+import br.com.cotiinformatica.dtos.MensagemUsuarioDto;
 import br.com.cotiinformatica.entities.Usuario;
 import br.com.cotiinformatica.repositories.UsuarioRepository;
 
@@ -27,6 +29,9 @@ public class UsuarioService {
 	@Autowired
 	JwtTokenComponent jwtTokenComponent;
 	
+	@Autowired
+	private RabbitMQProducerComponent rabbitMQProducerComponent;
+	
 	public CriarUsuarioResponseDto criar(CriarUsuarioRequestDto request) throws Exception {
 		
 		Usuario usuario = new Usuario();
@@ -39,6 +44,13 @@ public class UsuarioService {
 			throw new IllegalArgumentException("O email informado já está cadastrado. Tente outro.");
 		
 		usuarioRepository.save(usuario);
+		
+		MensagemUsuarioDto mensagem = new MensagemUsuarioDto();
+		mensagem.setEmailUsuario(usuario.getEmail());
+		mensagem.setAssunto("Conta de usuário criado com sucesso - COTI Informática");
+		mensagem.setTexto("Parabéns, " + usuario.getNome() + ", sua conta de usuário foi criada com sucesso.\n\nAtt\nEquipe COTI.");
+		
+		rabbitMQProducerComponent.sendMessage(mensagem);
 		
 		CriarUsuarioResponseDto response = new CriarUsuarioResponseDto();
 		response.setDataHoraCadastro(new Date());
